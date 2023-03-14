ab286749919461d5e0962de2d2dd71ada4bfbd76
i.chapaev <i.chapaev@smartway.today>
2023-03-14 22:50:32 +0300

    Initial commit

---
+++ CollectFilesToFoldersByType/Program.cs
@@ -0,0 +1,16 @@
+﻿var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+foreach (var file in Directory.EnumerateFiles(path))
+{
+    var extension = Path.GetExtension(file).Replace(".", "");
+    var extensionFolder = Path.Combine(path, extension);
+
+    if (!Directory.Exists(extensionFolder))
+    {
+        Directory.CreateDirectory(extensionFolder);
+    }
+
+    var filename = Path.GetFileName(file);
+    var newPath = Path.Combine(path, extension, filename);
+    File.Move(file,newPath, false);
+}
